2753942a64d5f3ba82d0758b950bc25ef08cfc04
singhsimranpal8 <76553385+singhsimranpal8@users.noreply.github.com>
2023-03-24 16:46:47 +0530

    Update newimage.py

--- newimage.py
+++ newimage.py
@@ -1,53 +1,59 @@
-# for ec2 to image
-import os
 import boto3
-from github3 import login
 
-
-instance_id = 'i-0d82a0e28c5411f99'
-amitag = 'copy ami from instance'
-
-#ec2 = boto3.resource('ec2')
+# Set up an EC2 client
 ec2 = boto3.client('ec2')
-instance = ec2.create_image(
-    InstanceId=instance_id,
-    Name=amitag,
-    Description='this is the AMI created from the python'
-)
-ami_id = instance['ImageId']
-waiter = ec2.get_waiter('image_available')
-waiter.wait(Filters=[{'Name': 'image-id', 'Values': [ami_id]}])
-print(f'AMI {ami_id} is now available.')
 
-# for image to ec2 
-ec2_client = boto3.client('ec2')
+# Define the tag that contains the AMI ID
+tag_name = 'AMI'
+tag_value = 'production'
 
-# Retrieve your GitHub access token from the environment variables
-github_access_token = os.environ.get('GITHUBACCESSTOKEN')
+# Get the AMI ID from the specified tag
+filters = [{'Name': 'tag:' + tag_name, 'Values': [tag_value]}]
+images = ec2.describe_images(Filters=filters)
 
-# Authenticate with the GitHub API using github3.py
-gh = login(token=github_access_token)
+if len(images['Images']) == 0:
+    print("No AMI found with the specified tag.")
+    exit()
 
-# Launch EC2 instance 
-response = ec2_client.run_instances(
-    ImageId=ami_id,  # Replace with your desired AMI ID
-    InstanceType='t2.micro',
-    KeyName='simran',  # Replace with the name of your key pair
+ami_id = images['Images'][0]['ImageId']
+
+# Create an EC2 instance using the specified AMI ID
+instance_type = 't2.micro'
+key_name = 'simran'
+security_group_id = 'sg-08a4f5b92da63cad7'
+subnet_id = 'subnet-0b1762b4023fbd3d0'
+
+response = ec2.run_instances(
+    ImageId=ami_id,
+    InstanceType=instance_type,
+    KeyName=key_name,
+    SecurityGroupIds=[security_group_id],
+    SubnetId=subnet_id,
     MinCount=1,
     MaxCount=1,
     UserData='''#!/bin/bash
                 git -C /var/www/html/bizhub-cicd-testing pull git@github.com:simranpal-webkul/bizhub-cicd-testing staging''')
 
-
-# Get instance ID
+# Get the instance ID of the new instance
 instance_id = response['Instances'][0]['InstanceId']
 
-# Set up EC2 waiter
-waiter = ec2_client.get_waiter('instance_running')
-
-# Wait for instance to become active
+# Wait for instance to be running
+print("Waiting for instance to be running...")
+waiter = ec2.get_waiter('instance_running')
 waiter.wait(InstanceIds=[instance_id])
+print("Instance is running.")
 
-instance = ec2_client.describe_instances(InstanceIds=[instance_id])
-print(f"Instance ID: {instance_id}")
-print(f"Public IP address: {instance['Reservations'][0]['Instances'][0]['PublicIpAddress']}")
+# Add the stage:staging tag to the new instance
+tag_key = 'stage'
+tag_value = 'staging'
+
+ec2.create_tags(Resources=[instance_id], Tags=[{'Key': tag_key, 'Value': tag_value}])
+
+print(f'Added tag {tag_key}:{tag_value} to instance {instance_id}')
+
+# Get the public IP address of the new instance
+instances = ec2.describe_instances(InstanceIds=[instance_id])
+public_ip = instances['Reservations'][0]['Instances'][0]['PublicIpAddress']
+
+print("Instance created with ID:", instance_id)
+print("Public IP address:", public_ip)
